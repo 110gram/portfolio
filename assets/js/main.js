63edac00358de8fdc6108eee5ae792f6cef2df81
110gram <87633450+110gram@users.noreply.github.com>
2021-10-14 18:51:24 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,26 @@ function general_utils() {
 		}, 1000);
 	});
 }
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('build1'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "assets/js/building1.json"
+  });
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('build2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: "assets/js/building2.json"
+});
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('build3'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: "assets/js/building3.json"
+});
